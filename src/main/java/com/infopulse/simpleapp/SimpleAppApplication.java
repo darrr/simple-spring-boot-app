f0196c1aeb2d52b6b4a0102129d95dc4987f89bd
Daria.Lazurenko <darrr2013@gmail.com>
2019-08-07 16:19:59 +0300

    service discovery

--- src/main/java/com/infopulse/simpleapp/SimpleAppApplication.java
+++ src/main/java/com/infopulse/simpleapp/SimpleAppApplication.java
@@ -2,12 +2,14 @@ package com.infopulse.simpleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(scanBasePackages = "com.infopulse.simpleapp")
 @EnableJpaRepositories(basePackages = "com.infopulse.simpleapp.repository")
 @EnableTransactionManagement
+@EnableDiscoveryClient
 public class SimpleAppApplication { public static void main(String[] args) {
     SpringApplication.run(SimpleAppApplication.class, args);
   }
